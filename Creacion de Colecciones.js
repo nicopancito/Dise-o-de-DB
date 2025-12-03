7ddc545b1e41581b1b6724002da74a7b9b6ae5c5
nicopancito <nicosarango10@gmail.com>
2025-12-03 18:34:39 -0500

    Update Creacion de Colecciones.js

--- Creacion de Colecciones.js
+++ Creacion de Colecciones.js
@@ -1,77 +1,75 @@
-use ("test");
-db.createCollection("usuario", {
-  validator: {
-    $jsonSchema: {
-      bsonType: "object",
-      required: ["nombre", "apellidoPaterno", "email", "hashPassword", "fechaNacimiento", "pais", "nivelSuscripcion", "estadoCuenta"],
-      properties: {
-        nombre: { bsonType: "string" },
-        apellidoPaterno: { bsonType: "string" },
-        apellidoMaterno: { bsonType: "string" },
-        email: { bsonType: "string" },
-        hashPassword: { bsonType: "string" },
-        fechaNacimiento: { bsonType: "string" }, // puedes usar ISODate si quieres
-        pais: { bsonType: "string" },
-        telefono: { bsonType: "string" },
-        nivelSuscripcion: { enum: ["gratis", "premium", "vip"] },
-        estadoCuenta: { enum: ["activo", "inactivo"] }
-      }
-    }
-  }
-});
+use ("ThriveAr"); 
 
-use ("test");
 db.createCollection("psicologo", {
   validator: {
     $jsonSchema: {
       bsonType: "object",
-      required: ["nombre", "apellidoPaterno", "email", "licencia", "centro", "estado", "verificado"],
+      required: ["nombre", "apellidoPaterno", "apellidoMaterno", "email", "licencia", "centro", "estado", "verificado"],
       properties: {
         nombre: { bsonType: "string" },
         apellidoPaterno: { bsonType: "string" },
         apellidoMaterno: { bsonType: "string" },
-        email: { bsonType: "string" },
+        email: { bsonType: "string", pattern: "^.+@.+$" },
         licencia: { bsonType: "string" },
         centro: { bsonType: "string" },
-        estado: { enum: ["disponible", "en_revision", "ocupado"] },
+        estado: { bsonType: "string" },
         verificado: { bsonType: "bool" }
       }
     }
   }
-});
+})
 
-use ("test");
-db.createCollection("evaluacion_clinica", {
-  validator: {
-    $jsonSchema: {
-      bsonType: "object",
-      required: ["usuarioId", "psicologoId", "fechaEvaluacion", "motivoConsulta", "diagnostico", "recomendaciones", "nivelRiesgo"],
-      properties: {
-        usuarioId: { bsonType: "objectId" },
-        psicologoId: { bsonType: "objectId" },
-        fechaEvaluacion: { bsonType: "date" },
-        motivoConsulta: { bsonType: "string" },
-        diagnostico: { bsonType: "string" },
-        recomendaciones: { bsonType: "string" },
-        nivelRiesgo: { enum: ["bajo", "medio", "alto"] }
-      }
-    }
-  }
-});
-
-use ("test");
 db.createCollection("cita", {
   validator: {
     $jsonSchema: {
       bsonType: "object",
-      required: ["usuarioId", "psicologoId", "fechaCita", "modalidad", "estado"],
+      required: ["fechaCita", "modalidad", "estado", "paciente", "psicologo"],
       properties: {
-        usuarioId: { bsonType: "objectId" },
-        psicologoId: { bsonType: "objectId" },
         fechaCita: { bsonType: "date" },
-        modalidad: { enum: ["virtual", "presencial"] },
-        estado: { enum: ["programada", "cancelada", "finalizada"] }
+        modalidad: { bsonType: "string" },
+        estado: { bsonType: "string" },
+        paciente: { bsonType: "object" },
+        psicologo: { bsonType: "object" }
       }
     }
   }
-});
+})
+
+db.createCollection("evaluacion_clinica", {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      required: ["fechaEvaluacion", "motivoConsulta", "diagnostico", "recomendaciones", "nivelRiesgo", "paciente", "psicologo"],
+      properties: {
+        fechaEvaluacion: { bsonType: "date" },
+        motivoConsulta: { bsonType: "string" },
+        diagnostico: { bsonType: "string" },
+        recomendaciones: { bsonType: "string" },
+        nivelRiesgo: { bsonType: "string" },
+        paciente: { bsonType: "object" },
+        psicologo: { bsonType: "object" }
+      }
+    }
+  }
+})
+
+
+db.createCollection("usuario", {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      required: ["nombre", "apellidoPaterno", "apellidoMaterno", "email", "fechaNacimiento", "pais", "telefono", "nivelSuscripcion", "estadoCuenta"],
+      properties: {
+        nombre: { bsonType: "string"},
+        apellidoPaterno: { bsonType: "string"},
+        apellidoMaterno: { bsonType: "string"},
+        email: { bsonType: "string", pattern: "^.+@.+$"},
+        fechaNacimiento: { bsonType: "string"},
+        pais: { bsonType: "string"},
+        telefono: { bsonType: "string"},
+        nivelSuscripcion: { bsonType: "string"},
+        estadoCuenta: { bsonType: "string"}
+      }
+    }
+  }
+})
